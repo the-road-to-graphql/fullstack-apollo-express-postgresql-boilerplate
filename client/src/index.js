1d5b7f753d9d539335d247ae9f0a9334705650e3
Robin Wieruch <wrobin@gmx.net>
2018-05-21 15:39:00 +0800

    subscription setup client

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { ApolloLink, split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
 import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
@@ -14,6 +16,24 @@ import registerServiceWorker from './registerServiceWorker';
 const httpLink = new HttpLink({
   uri: 'http://localhost:8000/graphql',
 });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8000/subscriptions`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const terminatingLink = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' && operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const authLink = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => ({
@@ -46,7 +66,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   }
 });
 
-const link = ApolloLink.from([authLink, errorLink, httpLink]);
+const link = ApolloLink.from([authLink, errorLink, terminatingLink]);
 
 const cache = new InMemoryCache();
 
