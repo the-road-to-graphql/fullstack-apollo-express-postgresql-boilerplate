a5fa9649c18a01825f391ee677cec56cb46e50ae
Robin Wieruch <wrobin@gmx.net>
2018-09-14 11:18:41 +0800

    cascade delete user

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -33,7 +33,7 @@ const user = (sequelize, DataTypes) => {
   });
 
   User.associate = models => {
-    User.hasMany(models.Message);
+    User.hasMany(models.Message, { onDelete: 'CASCADE' });
   };
 
   User.findByLogin = async login => {
