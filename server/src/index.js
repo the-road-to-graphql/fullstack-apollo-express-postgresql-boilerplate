469080f810a0049442f02393fae746cebc391cc0
Robin Wieruch <wrobin@gmx.net>
2018-10-25 13:02:45 +0800

    02-Apollo Server: Type Definitions

--- server/src/index.js
+++ server/src/index.js
@@ -9,20 +9,40 @@ app.use(cors());
 
 const schema = gql`
   type Query {
+    users: [User!]
     me: User
+    user(id: ID!): User
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+const me = users[1];
+
 const resolvers = {
   Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
     me: () => {
-      return {
-        username: 'Robin Wieruch',
-      };
+      return me;
     },
   },
 };
